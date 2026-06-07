b98c259b1daf876f199df9ee41544e0746bbe205
Hunter Moricz <ham8@sfu.ca>
2026-06-07 15:37:05 -0700

    Update function docstrings to NumPy style

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,17 @@
 def is_power_of_two(n: int) -> bool:
-    """ Returns true if the input is a power of two, false otherwise. """
+    """
+    Checks whether the input is a power of two.
+    
+    Parameters
+    ----------
+    n : int
+        The number under scrutiny.
+    
+    Returns
+    -------
+    bool
+        Returns true if the input is a power of two; otherwise, returns false.
+    """
     if type(n) is not int:
         raise TypeError('Input must be an integer')
     if n <= 0:
@@ -9,7 +21,26 @@ def is_power_of_two(n: int) -> bool:
     return n == 1
 
 def log2_int(n: int) -> int:
-    """ Returns the base-2 logarithm as an integer of the input, which must be a power of 2. """
+    """
+    Returns the base-2 logarithm as an integer of the input, which must be a power of 2. 
+    
+    Parameters
+    ----------
+    n : int
+        The integer to take the logarithm of.
+
+    Returns
+    -------
+    int
+        The base-two logarithm of the input value.
+
+    Raises
+    ------
+    TypeError
+        If the input is not an integer.
+    ValueError
+        If the input is not a power of two.
+    """
     if type(n) is not int:
         raise TypeError('Input must be an integer')
     if not is_power_of_two(n):
