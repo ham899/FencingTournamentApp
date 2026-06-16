9e955ffd3d45d8573d5b9c183e489119fc04de8a
Hunter Moricz <ham8@sfu.ca>
2026-06-16 14:48:50 -0700

    Refine entity classes (fencer & tournament entry) documentation, organization, and code layout, and place them in an "entities" folder

---
+++ src/entities/fencer.py
@@ -0,0 +1,124 @@
+from dataclasses import dataclass
+from typing import ClassVar
+
+@dataclass
+class Fencer:
+    """
+    Represents a fencer with an ID and a display name; a single fencer can participate in multiple tournaments (be multiple entries).
+    
+    Attributes
+    ----------
+    id : int
+        A unique identifier for the fencer.
+
+    display_name : str
+        The name of the fencer to be displayed in the tournament; leading and trailing whitespace will be trimmed from the input.
+        
+    max_name_length : ClassVar[int]
+        A class variable that defines the maximum allowed length for a fencer's display name; it is set to 25 characters.
+    """
+    id: int
+    display_name: str
+    
+    max_name_length: ClassVar[int] = 25
+
+    # --- Initialization and Validation ---
+    def __post_init__(self) -> None:
+        """
+        Validates the fencer's ID and display name upon initialization. 
+        The display name is also trimmed of leading and trailing whitespace; the trimmed name is used for input string length.
+
+        Raises
+        ------
+        TypeError
+            If the fencer's ID is not an integer or if the display name is not a string.
+        ValueError
+            If the fencer's ID is not a positive integer or if the display name is empty or exceeds the maximum allowed length after trimming.
+        """
+        if type(self.id) is not int:
+            raise TypeError('A fencer\'s ID must be an integer')
+
+        if self.id < 1:
+            raise ValueError('Fencer ID must be a positive integer')
+
+        validated_name = Fencer._validate_display_name(self.display_name)
+        
+        # Set valid display name
+        self.display_name = validated_name
+
+    # --- Dunder Methods ---
+    def __eq__(self, other: object) -> bool:
+        """
+        Determines if two fencers are equal based on their IDs.
+        It is based on IDs only since display names are allowed to change.
+
+        Parameters
+        ----------
+        other : object
+            The object to compare with the current fencer.
+
+        Returns
+        -------
+        bool
+            True if the other object is a Fencer with the same ID, False otherwise.
+        """
+        if not isinstance(other, Fencer):
+            return False
+        return self.id == other.id
+
+
+    # --- Public Methods ---
+    def update_display_name(self, name: str) -> None:
+        """
+        Updates the display name of the fencer.
+
+        Parameters
+        ----------
+        name : str
+            The new display name for the fencer; leading and trailing whitespace will be trimmed from the input.
+
+        Raises
+        ------
+        TypeError
+            If the new display name is not a string.
+        ValueError
+            If the new display name is empty or exceeds the maximum allowed length after trimming.
+        """
+        # Set new display name
+        self.display_name = Fencer._validate_display_name(name)
+
+    
+    # --- Private Helper Methods ---
+    @staticmethod
+    def _validate_display_name(name: str) -> str:
+        """
+        Validates a display name by trimming leading and trailing whitespace and checking that the resulting name is a valid length.
+
+        Parameters
+        ----------
+        name : str
+            The display name to validate.
+
+        Returns
+        -------
+        str
+            The validated display name with leading and trailing whitespace removed.
+
+        Raises
+        ------
+        TypeError
+            If the input name is not a string.
+        ValueError
+            If the input name is empty or exceeds the maximum allowed length after trimming.
+        """
+        if not isinstance(name, str):
+            raise TypeError('Display name must be a string')
+
+        # Trim input name
+        name = name.strip()
+
+        # Check that name is a valid length
+        if len(name) == 0 or len(name) > Fencer.max_name_length:
+            raise ValueError(f'Name must be between 1-{Fencer.max_name_length} characters inclusive, ignoring leading and trailing whitespace')
+        
+        return name
---
+++ src/entities/tournament_entry.py
@@ -0,0 +1,150 @@
+from dataclasses import dataclass
+
+from entities.fencer import Fencer
+
+@dataclass
+class TournamentEntry:
+    """
+    Represents an entry participating in a tournament.
+    An entry is a fencer in a specific tournament; one entry can only be associated with one fencer and one tournament.
+
+    Attributes
+    ----------
+    id : int
+        Unique identifier for the tournament entry.
+
+    tournament_id : int
+        Identifier for the tournament this entry is associated with.
+
+    fencer : Fencer
+        The fencer associated with this tournament entry.
+
+    initial_seed : int | None
+        The initial seed of the entry, which may be assigned upon initialization. 
+        The seed values do not have to have a one-to-one mapping with the number of entries upon initialization.
+        The one-to-one mapping will be inferred after initialization but before poules are generated.
+
+    de_seed : int | None
+        The DE seed of the entry, which should only be assigned if the tournament is a DE only event; otherwise, it should not be assigned. 
+        If assigned for a non-DE only event, it will be reassigned after poules are completed.
+    """
+    id: int
+    tournament_id: int
+    fencer: Fencer
+
+    initial_seed: int | None = None
+    de_seed: int | None = None
+
+    @property
+    def display_name(self) -> str:
+        """Gets the display name of the entry."""
+        return self.fencer.display_name
+
+    # --- Initialization and Validation ---
+    def __post_init__(self) -> None:
+        """
+        Validates the entry ID and tournament ID to be positive integers. Ensures the supplied fencer 
+        is a fencer object. Checks that provided seeds are positive integers.
+
+        Raises
+        ------
+        TypeError
+            If any attribute is an invalid type.
+        ValueError
+            If any of the numeric attributes are not positive integers.
+        """
+        # Validate types
+        if type(self.id) is not int:
+            raise TypeError('ID must be an integer')
+        
+        if type(self.tournament_id) is not int:
+            raise TypeError('Tournament ID must be an integer')
+        
+        if not isinstance(self.fencer, Fencer):
+            raise TypeError('Fencer parameter must be an instance of a Fencer class')
+        
+        if self.initial_seed is not None and type(self.initial_seed) is not int:
+            raise TypeError('Provided initial seed must be an integer')
+
+        if self.de_seed is not None and type(self.de_seed) is not int:
+            raise TypeError('Provided DE seed must be an integer')
+
+        # Validate values
+        if self.id <= 0:
+            raise ValueError('ID must be a positive integer')
+        
+        if self.tournament_id <= 0:
+            raise ValueError('Tournament ID must be a positive integer')
+
+        if self.initial_seed is not None and self.initial_seed <= 0:
+            raise ValueError('Provided initial seed must be a positive integer')
+
+        if self.de_seed is not None and self.de_seed <= 0:
+            raise ValueError('Provided DE seed must be a positive integer')
+
+    # --- Dunder Methods ---
+    def __eq__(self, other: object) -> bool:
+        """
+        Determines if two tournament entries are equal based on their entry IDs, tournament IDs, and the fencers associated with the objects.
+        Seeds are not considered in the equality check because they may change after initialization.
+
+        Parameters
+        ----------
+        other : object
+            The object to compare with this tournament entry.
+
+        Returns
+        -------
+        bool
+            True if the other object is a TournamentEntry with the same ID, False otherwise.
+        """
+        if not isinstance(other, TournamentEntry):
+            return False
+        return self.id == other.id and self.tournament_id == other.tournament_id
+
+    # --- Public Methods ---
+    def set_initial_seed(self, seed: int | None) -> None:
+        """
+        Sets a new initial seed of the entry.
+        
+        Parameters
+        ----------
+        seed : int | None
+            The initial seed to assign to the entry. Must be a positive integer or None.
+
+        Raises
+        ------
+        TypeError
+            If the seed is not an integer.
+        ValueError
+            If the seed is not a positive integer.
+        """
+        if seed is not None and type(seed) is not int:
+            raise TypeError(f'Provided seed must be an integer when setting initial seed for entry with ID {self.id}; got {type(seed)} instead')
+        if seed is not None and seed <= 0:
+            raise ValueError(f'Provided seed must be a positive integer when setting initial seed for entry with ID {self.id}; got {seed} instead')
+
+        self.initial_seed = seed
+
+    def set_de_seed(self, seed: int | None) -> None:
+        """
+        Sets a new DE seed of the entry.
+
+        Parameters
+        ----------
+        seed : int | None
+            The DE seed to assign to the entry. Must be a positive integer or None.
+
+        Raises
+        ------
+        TypeError
+            If the seed is not an integer.
+        ValueError
+            If the seed is not a positive integer.
+        """
+        if seed is not None and type(seed) is not int:
+            raise TypeError(f'Provided seed must be an integer when setting a DE seed for entry with ID {self.id}; got {type(seed)} instead')
+        if seed is not None and seed <= 0:
+            raise ValueError(f'Provided seed must be a positive integer when setting a DE seed for entry with ID {self.id}; got {seed} instead')
+
+        self.de_seed = seed
