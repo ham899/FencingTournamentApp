448c5796864175ab49ed2a350e1e839e0bfd5331
Hunter Moricz <ham8@sfu.ca>
2026-06-15 17:19:44 -0700

    Move poule/de match class helper function to the utils file for clarity and use in other classes if needed; move/create tests for these helper functions too

--- src/utils.py
+++ src/utils.py
@@ -50,3 +50,62 @@ def log2_int(n: int) -> int:
         n //= 2
         count+=1
     return count
+
+def calculate_number_poule_matches(poule_size: int) -> int:
+    """
+    Calculates the number of poule matches in a poule based on the poule's size.
+    """
+    if type(poule_size) is not int:
+        raise TypeError(f'The poule size input must be an integer - got {type(poule_size)}')
+    if poule_size < 2:
+        raise ValueError(f'There must be at least two people in a poule - got {poule_size}')
+    return poule_size*(poule_size-1) // 2
+
+def calculate_number_of_de_rounds(number_de_entries: int) -> int:
+    """
+    Calculates the number of rounds in a DE bracket based on the number of entries in the bracket.
+    """
+    if type(number_de_entries) is not int:
+        raise TypeError(f'The number of entries in the DE bracket must be an integer - got {type(number_de_entries)}')
+    if number_de_entries < 2:
+        raise ValueError(f'The number of entries in the DE bracket must be at least 2 - got {number_de_entries}')
+    i = 0
+    while not is_power_of_two(number_de_entries+i):
+        i+=1
+    return log2_int(n=number_de_entries+i)
+    
+def calculate_number_matches_in_de_round(round_index: int, number_de_entries: int) -> int:
+    """
+    Calculates the number of matches in a specific round in a DE bracket based on 
+    the round index and number of DE entries given.
+
+    Parameters
+    ----------
+    round_index : int
+        The round index under examination.
+    number_de_entries : int
+        The number of entrants in the DE tableau.
+    
+    Returns
+    -------
+    int
+        The number of matches in the round.
+
+    Raises
+    ------
+    TypeError
+        If the index inputs are not integers.
+    ValueError
+        If the index inputs are negative or if the round index input is greater than the maximum.
+    """
+    if type(round_index) is not int:
+        raise TypeError(f'The round index input must be an integer - got {type(round_index)}')
+    if type(number_de_entries) is not int:
+        raise TypeError(f'The number of DE entries input must be an integer - got {type(number_de_entries)}')
+    if number_de_entries < 2:
+        raise ValueError(f'The number of DE entries input must be at least 2 - got {number_de_entries}')
+    number_of_rounds = calculate_number_of_de_rounds(number_de_entries)
+    if round_index < 0 or round_index > number_of_rounds-1:
+        raise ValueError(f'The round index must be between 0 and {number_of_rounds-1} - got {round_index}')
+    
+    return 2**(number_of_rounds-round_index-1)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,15 @@
 from utils import is_power_of_two, log2_int
+from utils import calculate_number_poule_matches
+from utils import calculate_number_of_de_rounds, calculate_number_matches_in_de_round
 
 import pytest
 
-def test_is_power_of_two():
+### CONSTANTS ###
+NUMBER_DE_ENTRIES = 14
+
+### TESTS ###
+def test_is_power_of_two_valid():
+    assert is_power_of_two(-10) == False
     assert is_power_of_two(-1) == False
     assert is_power_of_two(0) == False
     assert is_power_of_two(1) == True
@@ -24,13 +31,12 @@ def test_is_power_of_two():
     assert is_power_of_two(501) == False
     assert is_power_of_two(512) == True
 
-def test_is_power_of_two_invalid():
+@pytest.mark.parametrize('invalid_n_type', [None, 'ten', True, 20.0, [], {}])
+def test_is_power_of_two_invalid_type(invalid_n_type):
     with pytest.raises(TypeError):
-        is_power_of_two('ten')
-    with pytest.raises(TypeError):
-        is_power_of_two(True)
+        is_power_of_two(invalid_n_type)
     
-def log2_int():
+def test_log2_int_valid():
     assert log2_int(1) == 0
     assert log2_int(2) == 1
     assert log2_int(4) == 2
@@ -43,38 +49,121 @@ def log2_int():
     assert log2_int(512) == 9
     assert log2_int(1024) == 10
     assert log2_int(2048) == 11
-    assert log2_int(5096) == 12
-    assert log2_int(10192) == 13
-    assert log2_int(20384) == 14
+    assert log2_int(4096) == 12
+    assert log2_int(8192) == 13
+    assert log2_int(16384) == 14
 
-def log2_int_invalid_type():
+@pytest.mark.parametrize('invalid_n_type', [None, 'zero', 8.0, True, [], {}])
+def test_log2_int_invalid_type(invalid_n_type):
     with pytest.raises(TypeError):
-        log2_int('zero')
-    with pytest.raises(TypeError):
-        log2_int([])
-    with pytest.raises(TypeError):
-        log2_int(-1.0)
-    with pytest.raises(TypeError):
-        log2_int(False)
+        log2_int(invalid_n_type)
 
-def log2_int_not_a_power_of_two():
+@pytest.mark.parametrize('not_a_power_of_two', [-1, -8, 0, 3, 5, 15, 25, 100, 511, 255])
+def test_log2_int_invalid_not_a_power_of_two(not_a_power_of_two):
     with pytest.raises(ValueError):
-        log2_int(3)
+        log2_int(not_a_power_of_two)
+
+def test_calculate_number_poule_matches_valid():
+    assert calculate_number_poule_matches(2) == 1
+    assert calculate_number_poule_matches(3) == 3
+    assert calculate_number_poule_matches(4) == 6
+    assert calculate_number_poule_matches(5) == 10
+    assert calculate_number_poule_matches(6) == 15
+    assert calculate_number_poule_matches(7) == 21
+    assert calculate_number_poule_matches(8) == 28
+    assert calculate_number_poule_matches(9) == 36
+    assert calculate_number_poule_matches(10) == 45
+    assert calculate_number_poule_matches(11) == 55
+    assert calculate_number_poule_matches(12) == 66
+    assert calculate_number_poule_matches(20) == 190
+
+@pytest.mark.parametrize('invalid_poule_size_type', [None, 7.0, 'six', False, [], {}])
+def test_calculate_number_poule_matches_invalid_input_type(invalid_poule_size_type):
+    with pytest.raises(TypeError):
+        calculate_number_poule_matches(invalid_poule_size_type)
+
+@pytest.mark.parametrize('invalid_poule_size_value', [-10, -1, 0, 1])
+def test_calculate_number_poule_matches_invalid_input_value(invalid_poule_size_value):
     with pytest.raises(ValueError):
-        log2_int(-1)
+        calculate_number_poule_matches(invalid_poule_size_value)
+
+def test_calculate_number_de_rounds_valid():
+    assert calculate_number_of_de_rounds(number_de_entries=2) == 1
+    assert calculate_number_of_de_rounds(number_de_entries=3) == 2
+    assert calculate_number_of_de_rounds(number_de_entries=4) == 2
+    assert calculate_number_of_de_rounds(number_de_entries=5) == 3
+    assert calculate_number_of_de_rounds(number_de_entries=6) == 3
+    assert calculate_number_of_de_rounds(number_de_entries=7) == 3
+    assert calculate_number_of_de_rounds(number_de_entries=8) == 3
+    assert calculate_number_of_de_rounds(number_de_entries=9) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=10) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=11) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=12) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=13) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=14) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=15) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=16) == 4
+    assert calculate_number_of_de_rounds(number_de_entries=17) == 5
+    assert calculate_number_of_de_rounds(number_de_entries=32) == 5
+    assert calculate_number_of_de_rounds(number_de_entries=33) == 6
+    assert calculate_number_of_de_rounds(number_de_entries=64) == 6
+    assert calculate_number_of_de_rounds(number_de_entries=65) == 7
+    assert calculate_number_of_de_rounds(number_de_entries=100) == 7
+    assert calculate_number_of_de_rounds(number_de_entries=200) == 8
+    assert calculate_number_of_de_rounds(number_de_entries=500) == 9
+    assert calculate_number_of_de_rounds(number_de_entries=1000) == 10
+
+@pytest.mark.parametrize('invalid_number_de_entries_type', [None, 100.0, True, 'thirty', [], {}])
+def test_calculate_number_of_de_rounds_invalid_number_de_entries_type(invalid_number_de_entries_type):
+    with pytest.raises(TypeError):
+        calculate_number_of_de_rounds(number_de_entries=invalid_number_de_entries_type)
+
+@pytest.mark.parametrize('invalid_number_de_entries_value', [-10, -1, 0, 1])
+def test_calculate_number_of_de_rounds_invalid_number_de_entries_value(invalid_number_de_entries_value):
     with pytest.raises(ValueError):
-        log2_int(-8)
+        calculate_number_of_de_rounds(number_de_entries=invalid_number_de_entries_value)
+
+def test_calculate_number_matches_in_de_round_valid():
+    assert calculate_number_matches_in_de_round(round_index=0, number_de_entries=2) == 1
+    assert calculate_number_matches_in_de_round(round_index=0, number_de_entries=4) == 2
+    assert calculate_number_matches_in_de_round(round_index=1, number_de_entries=4) == 1
+    assert calculate_number_matches_in_de_round(round_index=0, number_de_entries=8) == 4
+    assert calculate_number_matches_in_de_round(round_index=1, number_de_entries=8) == 2
+    assert calculate_number_matches_in_de_round(round_index=2, number_de_entries=8) == 1
+    assert calculate_number_matches_in_de_round(round_index=0, number_de_entries=16) == 8
+    assert calculate_number_matches_in_de_round(round_index=1, number_de_entries=16) == 4
+    assert calculate_number_matches_in_de_round(round_index=2, number_de_entries=16) == 2
+    assert calculate_number_matches_in_de_round(round_index=3, number_de_entries=16) == 1
+    assert calculate_number_matches_in_de_round(round_index=0, number_de_entries=32) == 16
+    assert calculate_number_matches_in_de_round(round_index=1, number_de_entries=32) == 8
+    assert calculate_number_matches_in_de_round(round_index=2, number_de_entries=32) == 4
+    assert calculate_number_matches_in_de_round(round_index=3, number_de_entries=32) == 2
+    assert calculate_number_matches_in_de_round(round_index=4, number_de_entries=32) == 1
+
+@pytest.mark.parametrize('invalid_round_index_type', [None, '0', 0.0, False, [], {}])
+def test_calculate_number_matches_in_de_round_invalid_round_index_type(invalid_round_index_type):
+    with pytest.raises(TypeError):
+        calculate_number_matches_in_de_round(round_index=invalid_round_index_type, number_de_entries=NUMBER_DE_ENTRIES)
+
+@pytest.mark.parametrize('invalid_round_index_negative', [-10, -1])
+def test_calculate_number_matches_in_de_round_invalid_round_index_negative(invalid_round_index_negative):
     with pytest.raises(ValueError):
-        log2_int(0)
+        calculate_number_matches_in_de_round(round_index=invalid_round_index_negative, number_de_entries=NUMBER_DE_ENTRIES)
+
+@pytest.mark.parametrize('invalid_number_de_entries_type', [None, 15.0, 'sixty', True, [], {}])
+def test_calculate_number_matches_in_de_round_invalid_number_de_entries_type(invalid_number_de_entries_type):
+    with pytest.raises(TypeError):
+        calculate_number_matches_in_de_round(round_index=0, number_de_entries=invalid_number_de_entries_type)
+
+@pytest.mark.parametrize('invalid_number_de_entries_value', [-10, -1, 0, 1])
+def test_calculate_number_matches_in_de_round_invalid_number_de_entries_value(invalid_number_de_entries_value):
     with pytest.raises(ValueError):
-        log2_int(5)
+        calculate_number_matches_in_de_round(round_index=0, number_de_entries=invalid_number_de_entries_value)
+
+def test_calculate_number_matches_in_de_round_invalid_round_index_greater_than_maximum():
     with pytest.raises(ValueError):
-        log2_int(15)
+        calculate_number_matches_in_de_round(round_index=10, number_de_entries=2)
     with pytest.raises(ValueError):
-        log2_int(25)
+        calculate_number_matches_in_de_round(round_index=4, number_de_entries=3)
     with pytest.raises(ValueError):
-        log2_int(100)
-    with pytest.raises(ValueError):
-        log2_int(511)
-    with pytest.raises(ValueError):
-        log2_int(255)
+        calculate_number_matches_in_de_round(round_index=3, number_de_entries=6)
