b97d80afb13ccd6b3bb4c3f55781bea730774309
Hunter Moricz <ham8@sfu.ca>
2026-06-18 17:28:50 -0700

    Apply rename to test file too

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,47 +1,47 @@
 import pytest
 
-import validators
+import validation
 
 INVALID_INT_TYPES = [None, 'five', 15.0, False, True, [], {}]
 
 @pytest.mark.parametrize('value', INVALID_INT_TYPES)
 def test_validate_int_rejects_non_ints(value):
     with pytest.raises(TypeError):
-        validators.validate_int(value, 'value', 'TestClass')
+        validation.validate_int(value, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [0, 5, 15])
 def test_validate_int_accepts_ints(value):
-    validators.validate_int(value, 'value', 'TestClass')
+    validation.validate_int(value, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [1, 5, 15])
 def test_validate_positive_int_accepts_positive_ints(value):
-    validators.validate_positive_int(value, 'value', 'TestClass')
+    validation.validate_positive_int(value, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [0, -1, -5, -15])
 def test_validate_positive_int_rejects_non_positive_ints(value):
     with pytest.raises(ValueError):
-        validators.validate_positive_int(value, 'value', 'TestClass')
+        validation.validate_positive_int(value, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [0, 5, 15])
 def test_validate_non_negative_int_accepts_zero_and_positive_ints(value):
-    validators.validate_non_negative_int(value, 'value', 'TestClass')
+    validation.validate_non_negative_int(value, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [-1, -5, -15])
 def test_validate_non_negative_int_rejects_negative_ints(value):
     with pytest.raises(ValueError):
-        validators.validate_non_negative_int(value, 'value', 'TestClass')
+        validation.validate_non_negative_int(value, 'value', 'TestClass')
 
 def test_validate_optional_positive_int_accepts_none():
-    validators.validate_optional_positive_int(None, 'value', 'TestClass')
+    validation.validate_optional_positive_int(None, 'value', 'TestClass')
 
 def test_validate_optional_non_negative_int_accepts_none():
-    validators.validate_optional_non_negative_int(None, 'value', 'TestClass')
+    validation.validate_optional_non_negative_int(None, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [0, 3, 5, 8, 12, 15])
 def test_validate_int_in_range_accepts_inclusive_bounds(value):
-    validators.validate_int_in_range(value, 0, 15, 'value', 'TestClass')
+    validation.validate_int_in_range(value, 0, 15, 'value', 'TestClass')
 
 @pytest.mark.parametrize('value', [-1, 16])
 def test_validate_int_in_range_rejects_out_of_range_values(value):
     with pytest.raises(ValueError):
-        validators.validate_int_in_range(value, 0, 15, 'value', 'TestClass')
+        validation.validate_int_in_range(value, 0, 15, 'value', 'TestClass')
