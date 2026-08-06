2bb5105ad5632bdf75987217defd568834de9a56
Hunter Moricz <ham8@sfu.ca>
2026-08-06 15:02:30 -0700

    Require input parameters to be tuples rather than iterables, also add one extra defensive check in _validate_matches

--- src/poules/results/poule_result.py
+++ src/poules/results/poule_result.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterable
 from dataclasses import dataclass, field, InitVar
 
 import validation
@@ -106,9 +105,9 @@ class PouleResult:
 
     Parameters
     ----------
-    poule_entries : Iterable[TournamentEntry]
+    poule_entries : tuple[TournamentEntry, ...]
         The entries whose results are calculated.
-    poule_matches : Iterable[PouleMatch]
+    poule_matches : tuple[PouleMatch, ...]
         The complete round-robin schedule from which results are derived.
     poule_id : int
         The unique identifier of the poule.
@@ -124,8 +123,8 @@ class PouleResult:
     tournament_id : int
         The unique identifier of the tournament containing the poule.
     """
-    poule_entries: InitVar[Iterable[TournamentEntry]]
-    poule_matches: InitVar[Iterable[PouleMatch]]
+    poule_entries: InitVar[tuple[TournamentEntry, ...]]
+    poule_matches: InitVar[tuple[PouleMatch, ...]]
 
     entry_results: tuple[PouleEntryResult, ...] = field(init=False)
     poule_id: int
@@ -133,49 +132,39 @@ class PouleResult:
     
     
     # --- Initialization and Validation Methods ---
-    def __post_init__(self, poule_entries: Iterable[TournamentEntry], poule_matches: Iterable[PouleMatch]) -> None:
+    def __post_init__(self, poule_entries: tuple[TournamentEntry, ...], poule_matches: tuple[PouleMatch, ...]) -> None:
         """
         Validate the inputs and calculate the entry results.
 
         Parameters
         ----------
-        poule_entries : Iterable[TournamentEntry]
+        poule_entries : tuple[TournamentEntry, ...]
             The entries belonging to the poule.
-        poule_matches : Iterable[PouleMatch]
+        poule_matches : tuple[PouleMatch, ...]
             The matches belonging to the poule.
 
         Raises
         ------
         TypeError
             If either ID is not an integer, if `poule_entries` or `poule_matches`
-            is not iterable, or if either iterable contains an object of the
-            wrong type.
+            is not a tuple, or if either tuple contains an object of the wrong type.
         ValueError
             If either ID is not positive, fewer than two entries are provided,
             entry IDs are not unique, an entry belongs to another tournament,
             the required number of matches is not provided, a match belongs to
-            another poule or tournament, a match contains an invalid entry, or
-            match IDs, match indices, or entry pairings are not unique.
+            another poule or tournament, a match contains the same entry twice, 
+            a match contains an invalid entry, or match IDs, match indices, 
+            or entry pairings are not unique.
         RuntimeError
             If a completed match does not have a valid winner index.
         """
         validation.validate_positive_int(self.tournament_id, 'Tournament ID', 'PouleResult')
         validation.validate_positive_int(self.poule_id, 'Poule ID', 'PouleResult')
-        
-        if not isinstance(poule_entries, Iterable):
-            raise TypeError(f'Provided entries in PouleResult must be an iterable - got {type(poule_entries).__name__}')
-        
-        if not isinstance(poule_matches, Iterable):
-            raise TypeError(f'Provided poule matches in PouleResult must be an iterable - got {type(poule_matches).__name__}')
-        
-        # Convert entries and matches iterables into tuples
-        entries = tuple(poule_entries)
-        matches = tuple(poule_matches)
 
-        self._validate_entries(entries)
-        self._validate_matches(matches, entries)
+        self._validate_entries(poule_entries)
+        self._validate_matches(poule_matches, poule_entries)
                 
-        object.__setattr__(self, 'entry_results', self._calculate_results_from_matches(entries, matches))
+        object.__setattr__(self, 'entry_results', self._calculate_results_from_matches(poule_entries, poule_matches))
 
 
     # --- Properties ---
@@ -264,7 +253,7 @@ class PouleResult:
                     entry_stats = results_tracker[entry.id]
                     
                     entry_stats.add_match_info(
-                        is_victory = True if entry_index == winner_index else False, 
+                        is_victory = entry_index == winner_index, 
                         touches_scored = match.score1 if entry_index == 0 else match.score2, 
                         touches_received = match.score2 if entry_index == 0 else match.score1
                     )
@@ -358,11 +347,11 @@ class PouleResult:
         ValueError
             If no matches are provided, the number of matches does not form a
             complete round-robin schedule, a match belongs to another poule or
-            tournament, a match contains an invalid entry, or match IDs, match
-            indices, or entry pairings are not unique.
+            tournament, a match contains the same entry twice, a match contains an invalid entry, 
+            or match IDs, match indices, or entry pairings are not unique.
         """
         if not isinstance(matches, tuple):
-            raise TypeError(f'The provided matches must be in a tuple - got {type(matches).__name__}')
+            raise TypeError(f'The provided matches must be a tuple - got {type(matches).__name__}')
         
         if not matches:
             raise ValueError(f'There must be at least one match present - got {len(matches)} matches')
@@ -385,7 +374,7 @@ class PouleResult:
                 raise ValueError(f'The poule match at index {i} in matches does not have the same poule ID {match.poule_id} as the poule ID of this result container {self.poule_id}')
 
             if match.tournament_id != self.tournament_id:
-                raise ValueError(f'The poule match at index {i} in matches does not have the same tournament ID {match.tournament_id} as the tournament ID this result container {self.tournament_id}')
+                raise ValueError(f'The poule match at index {i} in matches does not have the same tournament ID {match.tournament_id} as the tournament ID of this result container {self.tournament_id}')
                         
             if match.id in seen_match_ids:
                 raise ValueError(f'Poule match {match.id} occurs more than once in matches')
@@ -393,9 +382,15 @@ class PouleResult:
             if match.match_index in seen_match_indices:
                 raise ValueError(f'Match index {match.match_index} occurs more than once in matches')
             
+            if match.entry1 == match.entry2:
+                raise ValueError(f'Poule match {match.id} cannot contain the same entry twice')
+
             for entry in match.entries:
                 if entry.id not in valid_entry_ids:
                     raise ValueError(f'Poule match {match.id} contains entry {entry.id}, which is not a valid entry ID in this poule result container')
+                
+                if entry not in entries:
+                    raise ValueError(f'Poule match {match.id} contains entry {entry.id}, which does not belong in this poule result container')
             
             entry_id_pair = frozenset((match.entry1.id, match.entry2.id))
 
