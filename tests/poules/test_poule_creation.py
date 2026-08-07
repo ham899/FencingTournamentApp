2c20adf16e640af94c2ac2955b9f9ff1e214f652
Hunter Moricz <ham8@sfu.ca>
2026-08-06 18:17:59 -0700

    Add more tests to comprehensively test poule creation

--- tests/poules/test_poule_creation.py
+++ tests/poules/test_poule_creation.py
@@ -1,6 +1,8 @@
 import copy
 import pytest
 
+import factories
+
 from constants import (
     FENCER_ID2,
     ENTRY_ID3,
@@ -17,6 +19,7 @@ from poules.poule import Poule
 
 # --- Constants ---
 POULE_NUMBER1, POULE_NUMBER2 = 1, 2
+NON_POSITIVE_INTS = [-10, -1, 0]
 INVALID_ID_TYPES = [None, 'ABC', 1.0, True, ['F81C3'], (1,), {}]
 
 
@@ -24,6 +27,10 @@ INVALID_ID_TYPES = [None, 'ABC', 1.0, True, ['F81C3'], (1,), {}]
 @pytest.fixture
 def entries(entry1, entry2, entry3, entry4, entry5, entry6, entry7):
     return (entry1, entry2, entry3, entry4, entry5, entry6, entry7)
+
+@pytest.fixture
+def poule(entries):
+    return Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER2, entries)
 
 
 # --- Initialization and Validation Tests ---
@@ -35,7 +42,7 @@ def test_poule_creation_valid(entries):
     assert poule.poule_number == POULE_NUMBER1
     assert poule.entries == entries
     assert poule.size == len(entries)
-    assert poule.matches is not None
+    assert isinstance(poule.matches, tuple)
 
     expected_number_matches = (len(entries) * (len(entries) - 1)) // 2
     assert poule.number_matches == expected_number_matches
@@ -45,7 +52,7 @@ def test_poule_creation_invalid_id_type(entries, invalid_id_type):
     with pytest.raises(TypeError):
         Poule(invalid_id_type, TOURNY_ID1, POULE_NUMBER1, entries)
 
-@pytest.mark.parametrize('invalid_id_value', [-10, -1, 0])
+@pytest.mark.parametrize('invalid_id_value', NON_POSITIVE_INTS)
 def test_poule_creation_invalid_id_value(entries, invalid_id_value):
     with pytest.raises(ValueError):
         Poule(invalid_id_value, TOURNY_ID1, POULE_NUMBER1, entries)
@@ -55,7 +62,7 @@ def test_poule_creation_invalid_tournament_id_type(entries, invalid_tournament_i
     with pytest.raises(TypeError):
         Poule(POULE_ID1, invalid_tournament_id_type, POULE_NUMBER1, entries)
 
-@pytest.mark.parametrize('invalid_tournament_id_value', [-10, -1, 0])
+@pytest.mark.parametrize('invalid_tournament_id_value', NON_POSITIVE_INTS)
 def test_poule_creation_invalid_tournament_id_value(entries, invalid_tournament_id_value):
     with pytest.raises(ValueError):
         Poule(POULE_ID1, invalid_tournament_id_value, POULE_NUMBER1, entries)
@@ -65,80 +72,105 @@ def test_poule_creation_invalid_poule_number_type(entries, invalid_poule_number_
     with pytest.raises(TypeError):
         Poule(POULE_ID1, TOURNY_ID1, invalid_poule_number_type, entries)
 
-@pytest.mark.parametrize('invalid_poule_number_value', [-10, -1, 0])
+@pytest.mark.parametrize('invalid_poule_number_value', NON_POSITIVE_INTS)
 def test_poule_creation_invalid_poule_number_value(entries, invalid_poule_number_value):
     with pytest.raises(ValueError):
         Poule(POULE_ID1, TOURNY_ID1, invalid_poule_number_value, entries)
 
-@pytest.mark.parametrize('invalid_entries_type', [None, 'Harry', 0, 1.0, True, False])
+@pytest.mark.parametrize('invalid_entries_type', [None, 'Harry', 0, 1.0, True, False, [0], {}])
 def test_poule_creation_invalid_entries_type(invalid_entries_type):
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeError, match='Entries must be a tuple'):
         Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, invalid_entries_type)
 
-@pytest.mark.parametrize('invalid_entries_entry_type', [None, False, 0, 1.0, 'Harry', [], {}, Fencer(FENCER_ID2, 'Steve')])
-def test_poule_creation_invalid_entries_entry_type(entry1, entry3, invalid_entries_entry_type):
-    list_with_invalid_entry_type = [entry1, invalid_entries_entry_type, entry3]
-    with pytest.raises(TypeError):
-        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, list_with_invalid_entry_type)
+@pytest.mark.parametrize('invalid_entry_type', [None, False, 0, 1.0, 'Harry', [], {}, Fencer(FENCER_ID2, 'Steve')])
+def test_poule_creation_invalid_entries_entry_type(entry1, entry3, invalid_entry_type):
+    entries_invalid_entry_type = (entry1, invalid_entry_type, entry3)
+    
+    with pytest.raises(TypeError, match='Each entry must be a TournamentEntry'):
+        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries_invalid_entry_type)
+
+def test_poule_creation_invalid_no_entries():
+    with pytest.raises(ValueError, match='at least two entries'):
+        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, ())
+
+def test_poule_creation_invalid_only_one_entry(entry1):
+    with pytest.raises(ValueError, match='at least two entries'):
+        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, (entry1,))
 
 def test_poule_creation_invalid_entry_does_not_belong_to_tournament(entry1, entry2, fencer3):
     entry_wrong_tournament = TournamentEntry(ENTRY_ID3, TOURNY_ID2, fencer3)
-    list_with_invalid_entry_tournament_id = [entry1, entry2, entry_wrong_tournament]
-    with pytest.raises(ValueError):
-        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, list_with_invalid_entry_tournament_id)
+    
+    entries_invalid_entry_tournament_id = (entry1, entry2, entry_wrong_tournament)
+    
+    with pytest.raises(ValueError, match='belongs to tournament'):
+        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries_invalid_entry_tournament_id)
 
-def test_poule_size_and_number_matches_properties(entries):
-    poule1 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries[:2])
-    assert poule1.size == 2
-    assert poule1.number_matches == 1
-    assert poule1.number_matches == len(poule1.matches)
+def test_poule_creation_invalid_duplicate_entry(entry1, entry2):
+    duplicate_entry = copy.deepcopy(entry1)
 
-    poule2 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries[:3])
-    assert poule2.size == 3
-    assert poule2.number_matches == 3
-    assert poule2.number_matches == len(poule2.matches)
+    with pytest.raises(ValueError, match='appears more than once'):
+        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, (entry1, entry2, duplicate_entry))
 
-    poule3 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries[:4])
-    assert poule3.size == 4
-    assert poule3.number_matches == 6
-    assert poule3.number_matches == len(poule3.matches)
+@pytest.mark.parametrize('unsupported_size', [13, 14, 15, 16])
+def test_poule_creation_invalid_unsupported_size(unsupported_size):
+    entries = factories.make_entries(n=unsupported_size, tournament_id=TOURNY_ID1, initial_seed=True)
 
-    poule4 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries[:5])
-    assert poule4.size == 5
-    assert poule4.number_matches == 10
-    assert poule4.number_matches == len(poule4.matches)
+    with pytest.raises(ValueError, match='no official bout order exists for that size'):
+        Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries)
 
-    poule5 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries[:6])
-    assert poule5.size == 6
-    assert poule5.number_matches == 15
-    assert poule5.number_matches == len(poule5.matches)
+@pytest.mark.parametrize(
+        ('size', 'expected_number_matches'),
+        [
+            (2, 1),
+            (3, 3),
+            (4, 6),
+            (5, 10),
+            (6, 15),
+            (7, 21),
+            (8, 28),
+            (9, 36),
+            (10, 45),
+            (11, 55),
+            (12, 66)
+        ]
+)
+def test_poule_size_and_number_matches_properties(size, expected_number_matches):
+    entries = factories.make_entries(n=size, tournament_id=TOURNY_ID1, initial_seed=True)
+    
+    poule = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries)
 
-    poule6 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries)
-    assert poule6.size == 7
-    assert poule6.number_matches == 21
-    assert poule6.number_matches == len(poule6.matches)
+    assert poule.size == size
+    assert poule.number_matches == expected_number_matches
+    assert poule.number_matches == len(poule.matches)
 
 def test_poule_equality(entries):
     poule1 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries)
+    
     poule2 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries)
+    
     assert poule1 == poule2
 
-def test_poule_equality_after_modification(poule):
+def test_poule_equality_ignores_match_state(poule):
     poule_copy = copy.deepcopy(poule)
+    
     poule.record_on_piste_match_result(5,3)
+    
     assert poule == poule_copy
+
+@pytest.mark.parametrize('other', [None, 1, 'Poule', (), object()])
+def test_poule_inequality_different_type(poule, other):
+    assert poule != other
 
 def test_poule_inequality_different_poules_same_tournament(entries):
     poule1 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries[0:3])
+    
     poule2 = Poule(POULE_ID2, TOURNY_ID1, POULE_NUMBER2, entries[3:6])
+    
     assert poule1 != poule2
 
 def test_poule_inequality_different_tournaments(entries):
     poule1 = Poule(POULE_ID1, TOURNY_ID1, POULE_NUMBER1, entries)
-
-    for entry in entries:
-        entry.tournament_id = TOURNY_ID2
     
-    poule2 = Poule(POULE_ID1, TOURNY_ID2, POULE_NUMBER1, entries)
+    poule2 = Poule(POULE_ID1, TOURNY_ID2, POULE_NUMBER1, factories.make_entries(n=7, tournament_id=TOURNY_ID2, initial_seed=True))
     
     assert poule1 != poule2
