64b8014ca9fea675949cb5834d1d6c5e8a73092e
Hunter Moricz <ham8@sfu.ca>
2026-08-12 14:57:09 -0700

    Make factory file clearer/simpler

--- tests/factories.py
+++ tests/factories.py
@@ -1,71 +1,19 @@
+# tests/factories.py
+
 from entities.fencer import Fencer
 from entities.tournament_entry import TournamentEntry
 from matches.poule_match import PouleMatch
 from poules.poule import Poule
 from poules.poule_orders import POULE_BOUT_ORDER
-from poules.poule_round import PouleRound
+from sample_names import SAMPLE_NAMES
 
-NAME_POOL = (
-    'John',
-    'Steve',
-    'Hannah',
-    'Emily',
-    'Michael',
-    'Sarah',
-    'Dave',
-    'Jill',
-    'Parsa',
-    'Chantelle',
-    'Jessica',
-    'Jack',
-    'Edward',
-    'Jane',
-    'Joanna',
-    'Stephen',
-    'Robert',
-    'Bob',
-    'Peter',
-    'Catherine',
-    'Rob',
-    'Isabella',
-    'Albert',
-    'Elizabeth',
-    'Victoria',
-    'Jeseph',
-    'Mary',
-    'William',
-    'James',
-    'Benjamin',
-    'Lucas',
-    'Mason',
-    'Ethan',
-    'Alexander',
-    'Daniel',
-    'Matthew',
-    'Henry',
-    'Sebastian',
-    'Owen',
-    'Gabriel',
-    'Carter',
-    'Wyatt',
-    'Caleb',
-    'Isaac',
-    'Ryan',
-    'Nathan',
-    'Aaron',
-    'Christian'
-)
 
 def make_fencer(fencer_id: int, name: str) -> Fencer:
     """Creates a valid Fencer for use in tests."""
     return Fencer(fencer_id, name)
 
-def make_tournament_entry(id: int, 
-                          fencer: Fencer, 
-                          tournament_id: int, 
-                          *, 
-                          initial_seed: int | None = None, 
-                          de_seed: int | None = None) -> TournamentEntry:
+def make_tournament_entry(id: int, fencer: Fencer, tournament_id: int, 
+                          *, initial_seed: int | None = None, de_seed: int | None = None) -> TournamentEntry:
     """Creates a valid TournamentEntry for use in tests."""
     return TournamentEntry(id, tournament_id, fencer, initial_seed, de_seed)
 
@@ -75,22 +23,14 @@ def make_entries(n: int, tournament_id: int, *, initial_seed: bool = False, de_s
         make_tournament_entry(
             id = i + 1, 
             tournament_id = tournament_id,
-            fencer = make_fencer(i + 1, NAME_POOL[i]), 
+            fencer = make_fencer(i + 1, SAMPLE_NAMES[i]), 
             initial_seed = i + 1 if initial_seed else None, 
             de_seed = i + 1 if de_seed else None
         ) for i in range(n)
     )
 
-def make_poule_match(
-        match_id: int, 
-        tournament_id: int,
-        entry1: TournamentEntry, 
-        entry2: TournamentEntry, 
-        poule_id: int, 
-        match_index: int,
-        *,
-        score1: int | None = None,
-        score2: int | None = None) -> PouleMatch:
+def make_poule_match(match_id: int, tournament_id: int, entry1: TournamentEntry, entry2: TournamentEntry, poule_id: int, match_index: int,
+                     *, score1: int | None = None, score2: int | None = None) -> PouleMatch:
     """Creates a valid uncompleted PouleMatch for use in tests."""
     poule_match = PouleMatch(
         id = match_id, 
@@ -106,13 +46,10 @@ def make_poule_match(
     
     return poule_match
 
-def make_poule_matches(
-        entries: tuple[TournamentEntry, ...], 
-        poule_id: int, 
-        tournament_id: int,
-        *,
-        scores: tuple[tuple[int, int], ...] = None) -> tuple[PouleMatch, ...]:
+def make_poule_matches(entries: tuple[TournamentEntry, ...], poule_id: int, tournament_id: int,
+                       *, scores: tuple[tuple[int, int], ...] = None) -> tuple[PouleMatch, ...]:
     """Creates a tuple of PouleMatch objects based on the official bout order."""
+    
     bout_order = POULE_BOUT_ORDER[len(entries)]
 
     matches = []
@@ -129,20 +66,15 @@ def make_poule_matches(
                 entry2 = entry2, 
                 poule_id = poule_id, 
                 match_index = i,
-                score1 = scores[i][0] if scores is not None else None,
-                score2 = scores[i][1] if scores is not None else None
+                score1 = scores[i][0] if scores is not None and i < len(scores) else None,
+                score2 = scores[i][1] if scores is not None and i < len(scores) else None
             )
         )
 
     return tuple(matches)
 
-def make_poule(
-        id: int, 
-        tournament_id: int,
-        poule_number: int, 
-        entries: tuple[TournamentEntry, ...],
-        *,
-        scores: tuple[tuple[int, int], ...] = None) -> Poule:
+def make_poule(id: int, tournament_id: int, poule_number: int, entries: tuple[TournamentEntry, ...], 
+               *, scores: tuple[tuple[int, int], ...] = None) -> Poule:
     """Creates a valid Poule for use in tests."""
     poule = Poule(id, tournament_id, poule_number, entries=entries)
 
@@ -151,8 +83,4 @@ def make_poule(
         for i, (score1, score2) in enumerate(scores):
             poule.record_match_result(i, score1, score2)
     
-    return poule 
-
-def make_poule_round(entries: tuple[TournamentEntry, ...], id, tournament_id, round_number) -> PouleRound:
-    """Creates a valid PouleRound for use in tests."""
-    return PouleRound(id, tournament_id, round_number, entries)
+    return poule
